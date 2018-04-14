a104b9b7cc2e39b4549f15b8c1b722d9cedfd435
MaattMaatt <36171616+MaattMaatt@users.noreply.github.com>
2018-04-13 20:14:42 -0400

    Defined objects block and point
    defined block and point.  should be final; I will now work on laser which will incorporate these two objects in it.

--- objects/block.py
+++ objects/block.py
@@ -1,17 +1,32 @@
+ 
+class block:
+    
+    def __init__(self,blocktype):
+        
+        # block contains boolean info about the ways a laser will interact
+        # laser can go go thru, be "killed", reflect, or reflect + refract
 
-class Block:
-    '''
-    A generic block for lazor.  We make this extendable so that it can be
-    defined as either:
+        if blocktype == 'x' or blocktype == 'o':
+            self.thru = 1
+            self.kill = 0
+            self.reflect = 0
+            self.refract = 0
+        elif blocktype == 'b' or blocktype == 'B':
+            self.thru = 0
+            self.kill = 1
+            self.reflect = 0
+            self.refract = 0
+        else: # reflect or refract
+            self.thru = 0
+            self.kill = 0
+            self.reflect = 1
+            if blocktype == 'c' or blocktype == 'C':
+                self.refract = 1
+            else:
+                self.refract = 0
 
-        (a) Reflecting block - Only reflects the laser
-        (b) Opaque block - Absorbs the laser
-        (c) See-Through block - Both reflects and lets light pass
-    '''
-    def __init__(self):
-        '''
-        Difficulty 1
-
-        DONT FORGET TO COMMENT!
-        '''
-        pass
+# opaq = block('o')
+# print opaq.thru 
+# print opaq.kill 
+# print opaq.reflect 
+# print opaq.refract 
--- objects/laser.py
+++ objects/laser.py
@@ -1,16 +1,16 @@
 
 class Laser:
-    '''
-    The Laser.  We need to store both the starting position and direction of
-    the laser.
-    '''
-    def __init__(self):
-        '''
-        Difficulty 1
 
-        DONT FORGET TO COMMENT!
-        '''
-        pass
+    def __init__(self,laserpos,laserdirection):
+        # set the starting position and direction
+        # position is 2-list of laser's coordinates
+        # direction is a 2-list of laser's unit velocity (ex: (-1,1))
+        self.position = laserpos
+        self.direction = laserdirection
 
+    def update(self,board_variation, all_points)
+        # find potential new location of laser and decide how it will interact with that point
+        newpoint
+    pass
     # MORE
     # Difficulty 4
