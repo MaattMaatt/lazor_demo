9c175dd7d3ceeb704f8918d3ee2323432cffeed4
MaattMaatt <36171616+MaattMaatt@users.noreply.github.com>
2018-04-14 23:04:38 -0400

    Made game(run)
    made game(run); resolved differences between laser and game.

--- objects/laser.py
+++ objects/laser.py
@@ -20,29 +20,27 @@ class Laser:
         if self.position[0] % 2 == 0:
             bpt = [self.position[0]+self.direction[0], self.position[1]]
         else:
-            bpt = [self.position[0], self.position[1] + self.direction[1]]
-            
+            bpt = [self.position[0], self.position[1] + self.direction[1]] 
         # associate bpt with position in board
         boardx = (bpt[0]-1)/2
         boardy = (bpt[1]-1)/2
-
         new_laser = None
         newpt2 = None
         newdir = None
-        if boardx < 0 or boardy < 0 or boardx > boardlen or boardy > boardht:
+        if boardx < 0 or boardy < 0 or boardx > boardlen-1 or boardy > boardht-1:
             self.killed = 1
         else:
             # decide how we interact based on block properties, update laser with "newpt"
-            thisblock = Block(thisboard[boardx][boardy]) # make block objects only as needed to save time
+            thisblock = Block(thisboard[boardy][boardx]) # make block objects only as needed to save time
             if thisblock.kill == 1:
                 self.killed = 1
             elif thisblock.thru == 1:
                 self.position = [self.position[0] + self.direction[0], self.position[1] + self.direction[1]]
                 # (direction unchanged if go through)
-            else: 
+            else:
                 if thisblock.refract == 1:
-                    newpt2 = [self.position[0]+self.direction[0], self.position[1] + self.direction[1]]
-                    newdir2 = self.direction
+                    newpt2 = [self.position[0] + self.direction[0], self.position[1] + self.direction[1]]
+                    newdir2 = [self.direction[0],self.direction[1]]
                     new_laser = newpt2,newdir2
                 # reflect: change direction; again, handle even and odd x-coordinate cases differently
                 if self.position[0] % 2 == 0:
@@ -53,19 +51,15 @@ class Laser:
         # update all_points if we hit a point
         for i in range(len(all_points)):
             all_points[i].check_intersection(self.position)
-        return new_laser # return None if we dont refract
+        return all_points,new_laser # return None if we dont refract
 
 # # test stuff
-# thisboard = [['C','o','o'],['o','o','o'],['o','o','o']]
-# pthit = [Point([2,3])]
-# l = Laser([0,3],[1,-1])
-# print l.update(thisboard,pthit,3,3)
+# thisboard = [['O','O','O','O'],['O','O','O','O'],['O','O','O','O'],['A','O','O','O'],['O','O','O','C'],['O','O','A','O']]
+# pthit = [Point([1,2]),Point([6,3])]
+# l = Laser([8,7],[-1,1])
+# l.update(thisboard,pthit,4,6)
 # print 'stuff:'
 # print l.position, l.direction
 # print l.killed
 # print ':stuff'
-# print l.update(thisboard,pthit,3,3)
-# print 'stuff:'
-# print l.killed
-# print l.position, l.direction
-# print ':stuff'
+
